24ddad38c140ad7dd92531814ecc8210908f7d36
BrotherB1 <bassettl@mail.gvsu.edu>
2020-02-19 08:10:26 -0500

    Have some basic enemy movement and complete animations

--- examples/game.py
+++ examples/game.py
@@ -7,6 +7,9 @@ import league
 from components import *
 from player import Player
 from overlay import Overlay
+from enemies.spider import Spider
+from enemies.bee import Bee
+from enemies.ant import Ant
 
 
 """This file is garbage. It was a hastily coded mockup
@@ -35,14 +38,39 @@ def main():
     p.blocks.add(t.impassable)
     p.world_size = world_size
     p.rect = p.image.get_rect()
+
     q = Player(10, 100, 100)
     q.image = p.image
+
+    s = Spider(10, 300, 300, 120)
+    s.blocks.add(t.impassable)
+    s.world_size = world_size
+    s.rect = s.image.get_rect()
+
+    b = Bee(10, 400, 400, 120)
+    b.blocks.add(t.impassable)
+    b.world_size = world_size
+    b.rect = b.image.get_rect()
+
+    a = Ant(10, 500, 300, 120)
+    a.blocks.add(t.impassable)
+    a.world_size = world_size
+    a.rect = a.image.get_rect()
+
     e.objects.append(p)
     e.objects.append(q)
+    e.objects.append(s)
+    e.objects.append(b)
+    e.objects.append(a)
+
     e.drawables.add(p)
     e.drawables.add(q)
+    e.drawables.add(s)
+    e.drawables.add(b)
+    e.drawables.add(a)
     e.drawables.add(o)
     e.drawables.add(bu)
+
     c = league.LessDumbCamera(800, 600, p, e.drawables, world_size)
     #c = league.DumbCamera(800, 600, p, e.drawables, world_size)
     
@@ -52,11 +80,18 @@ def main():
 
     e.collisions[p] = (q, p.ouch) 
     pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // league.Settings.gameTimeFactor)
+    pygame.time.set_timer(pygame.USEREVENT + 2, 150 // league.Settings.gameTimeFactor)
+    pygame.time.set_timer(pygame.USEREVENT + 3, 100 // league.Settings.gameTimeFactor)
+    pygame.time.set_timer(pygame.USEREVENT + 4, 500 // league.Settings.gameTimeFactor)
+
     e.key_events[pygame.K_a] = p.move_left
     e.key_events[pygame.K_d] = p.move_right
     e.key_events[pygame.K_w] = p.move_up
     e.key_events[pygame.K_s] = p.move_down
     e.events[pygame.USEREVENT + 1] = q.move_right
+    e.events[pygame.USEREVENT + 2] = s.move
+    e.events[pygame.USEREVENT + 3] = b.move
+    e.events[pygame.USEREVENT + 4] = a.move
     e.events[pygame.QUIT] = e.stop
     e.run()
 
