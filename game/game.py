acc91eda098e84162bab06f3ce6a091a08d1bc4d
BrotherB1 <bassettl@mail.gvsu.edu>
2020-02-23 15:19:22 -0500

    Finished merge with master.

--- game/game.py
+++ game/game.py
@@ -17,102 +17,6 @@ from components.overlay import Overlay_Button
 
 
 def main():
-# <<<<<<< HEAD
-#     e = league.Engine("Go Nutts!")
-#     e.init_pygame()
-
-#     ##sprites = league.Spritesheet('../assets/base_chip_pipo.png', league.Settings.tile_size, 8)
-#     backgroundMaterial = league.Spritesheet('../assets/woodlandMaterials.png', league.Settings.tile_size, 5)
-#     t = league.Tilemap('../assets/woodland.lvl', backgroundMaterial, layer = 1)
-#     b = league.Tilemap('../assets/background.lvl', backgroundMaterial, layer = 0)
-#     world_size = (t.wide*league.Settings.tile_size, t.high *league.Settings.tile_size)
-#     e.drawables.add(b.passable.sprites()) 
-#     e.drawables.add(t.passable.sprites())
-#     m = SoundManager()
-#     m.bgm_start('Song_For_Someone.wav')
-#     p = Player(2, 400, 100)
-#     o = Overlay(p)
-#     bu = MusicButton()
-#     p.blocks.add(t.impassable)
-#     p.world_size = world_size
-#     p.rect = p.image.get_rect()
-
-#     q = Player(10, 100, 100)
-#     q.image = p.image
-
-#     s = Spider(10, 250, 50, 70)
-#     s.blocks.add(t.impassable)
-#     s.world_size = world_size
-#     s.rect = s.image.get_rect()
-
-#     b = Bee(10, 200, 100, 120)
-#     b.blocks.add(t.impassable)
-#     b.world_size = world_size
-#     b.rect = b.image.get_rect()
-
-#     ac = Acorn(10, 150, 100)
-#     ac.blocks.add(t.impassable)
-#     ac.world_size = world_size
-#     ac.rect = ac.image.get_rect()
-
-
-
-#     a = Leafbug(10, 100, 200, 120)
-#     a.blocks.add(t.impassable)
-#     a.world_size = world_size
-#     a.rect = a.image.get_rect()
-
-#     e.objects.append(p)
-#     e.objects.append(q)
-#     e.objects.append(s)
-#     e.objects.append(b)
-#     e.objects.append(a)
-#     e.objects.append(ac)
-
-#     e.drawables.add(p)
-#     e.drawables.add(q)
-#     e.drawables.add(s)
-#     e.drawables.add(b)
-#     e.drawables.add(a)
-#     e.drawables.add(ac)
-#     e.drawables.add(o)
-#     e.drawables.add(bu)
-
-#     c = league.LessDumbCamera(800, 400, p, e.drawables, world_size)
-#     #c = league.DumbCamera(800, 600, p, e.drawables, world_size)
-    
-#     e.objects.append(c)
-#     e.objects.append(o)
-#     e.objects.append(bu)
-
-#     e.collisions[p] = (q, p.ouch) 
-#     pygame.time.set_timer(pygame.USEREVENT + 1, 20 // league.Settings.gameTimeFactor)
-#     pygame.time.set_timer(pygame.USEREVENT + 2, 125 // league.Settings.gameTimeFactor)
-#     pygame.time.set_timer(pygame.USEREVENT + 3, 100 // league.Settings.gameTimeFactor)
-#     pygame.time.set_timer(pygame.USEREVENT + 4, 100 // league.Settings.gameTimeFactor)
-#     pygame.time.set_timer(pygame.USEREVENT + 5, 20 // league.Settings.gameTimeFactor)
-
-#     e.key_events[pygame.K_a] = p.move_left
-#     e.key_events[pygame.K_d] = p.move_right
-#     # e.key_events[pygame.K_w] = p.move_up
-#     # e.key_events[pygame.K_s] = p.move_down
-#     e.add_key(pygame.K_a, p.move_left)
-#     e.add_key(pygame.K_d, p.move_right)
-
-#     # e.add_key(pygame.K_s, p.move_down)
-#     e.add_key(pygame.K_b, p.climb_on)
-#     e.add_key(pygame.K_SPACE, p.jump)
-
-#     e.add_key(pygame.K_COMMA, p.reset_position)
-
-#     e.events[pygame.USEREVENT + 1] = p.move_down #gravity
-#     e.events[pygame.USEREVENT + 5] = p.update_jump
-
-
-
-#     e.events[pygame.QUIT] = e.stop
-#     e.run()
-# =======
     gamin = True
     while(gamin):
         gamin = False
@@ -289,20 +193,9 @@ def main():
 
         e.add_key(pygame.K_a, p.move_left)
         e.add_key(pygame.K_d, p.move_right)
-        e.add_key(pygame.K_w, p.move_up)
-        e.add_key(pygame.K_s, p.move_down)
-        e.add_key(pygame.K_b, p.climb_on)
+
         e.add_key(pygame.K_q, p.print_place)
-        e.add_key(pygame.K_SPACE, p.climb_off)
 
-        # e.key_events[pygame.K_a] = p.move_left
-        # e.key_events[pygame.K_d] = p.move_right
-        # e.key_events[pygame.K_w] = p.move_up
-        # e.key_events[pygame.K_s] = p.move_down
-        e.add_key(pygame.K_a, p.move_left)
-        e.add_key(pygame.K_d, p.move_right)
-
-        # e.add_key(pygame.K_s, p.move_down)
         e.add_key(pygame.K_b, p.climb_on)
         e.add_key(pygame.K_SPACE, p.jump)
 
@@ -327,7 +220,6 @@ def main():
         e.run()
         if e.reset:
             gamin = True
-# >>>>>>> master
 
 if __name__=='__main__':
     main()
